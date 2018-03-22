de5e69f34658a79e3945091bd91f9088d5240fd0
Yonatan Lehman <yonatan.lehman@applitools.com>
2018-03-22 14:06:36 +0200

    added checkpoint after multiply

--- test/e2e/test-app.js
+++ test/e2e/test-app.js
@@ -100,6 +100,7 @@ describe('calculator app', function () {
       await digit4Element.click()
       await digit2Element.click()
       await operatorMultiply.click()
+	  await eyes.checkWindow('After multiply')
       await digit2Element.click()
       await operatorEquals.click()
 
